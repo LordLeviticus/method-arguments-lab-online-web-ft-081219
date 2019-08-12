7069ad21987a0734ad6633e0c4d4c8ef0ebc653a
Levi Borland <borland.levi@gmail.com>
2019-08-12 21:37:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,2 +1,7 @@
-def introduction
+def introduction_with_language(name, language)
+  puts "He, my name is #{name} and I am learning to program in #{language}."
+end
+
+def introduction(name)
+  puts "Hi, my name is #{name}."
 end
